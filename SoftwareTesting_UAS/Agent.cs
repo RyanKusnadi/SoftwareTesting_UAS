8ea9f64e3391a0aa6455e499581c6e8260411238
ryank <ryank@DESKTOP-8F258HT>
2022-05-25 18:02:39 +0700

    Add project files.

---
+++ SoftwareTesting_UAS/Agent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoftwareTesting_UAS
+{
+    internal class Agent
+    {
+        //public prefab Projectile_Prefab;
+        public Agent(string label, string name, int maximumHealthPoint, int currentHealthPoint, int attackPoint)
+        {
+            Label = label;
+            Name = name;
+            MaximumHealthPoint = maximumHealthPoint;
+            CurrentHealthPoint = currentHealthPoint;
+            AttackPoint = attackPoint;
+        }
+
+        public string Label {get; set;}
+        public string Name { get; set; }
+        public int MaximumHealthPoint { get; set; }
+        public int CurrentHealthPoint { get; set; }
+        public int AttackPoint { get; set; }
+        
+        public int Ability1(Agent collidingAgent)//400 (+60% Total Attack Point) (+target's 30% Lost HP) true damage. If boss, max = 2500
+        {
+            int baseDamage = 400;
+            int percentageBasedDamage = (int)(0.3 * collidingAgent.MaximumHealthPoint - CurrentHealthPoint);
+            int attackPointDamage = (int)(0.6 * AttackPoint);
+            int total = baseDamage + percentageBasedDamage + attackPointDamage;
+
+            if (collidingAgent.Label == "Boss")
+            {
+                return Math.Min(2500, total);
+            }
+            else
+            {
+               return total;
+            }
+        }
+    }
+}
---
+++ SoftwareTesting_UAS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftwareTesting_UAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //instansiasi agent bernama player, attack pointnya 300
+            Agent player = new Agent("Player", "Player1", 500, 500, 300);
+            
+            //instansiasi agent bernama enemy1, labelnya Boss, missing HPnya = 3500 (max-current)
+            Agent Enemy1 = new Agent("Boss", "Turtle", 5000, 1500, 100);
+            Agent Enemy2 = new Agent("Boss", "Lord", 45000, 5000, 100);
+
+            int damage = player.Ability1(Enemy1); //400 + (60%*300) + (30%*3500) = 1580
+            int damage2 = player.Ability1(Enemy2); //400 + (60%*300) + (30%*40000) = 12580, tp boss, jd max 2500
+            Console.WriteLine(damage); //1580
+            Console.WriteLine(damage2); //2500
+
+        }
+    }
+}
